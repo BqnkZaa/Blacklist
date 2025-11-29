c990e4cb933cbbc23e0203645c4a654798be06c3
badree <ayumguring093@gmail.com>
2025-11-29 23:09:10 +0700

    Update API URL to ngrok

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://miracidial-arboreally-tanisha.ngrok-free.dev'  // แทนที่ด้วย URL จาก ngrok
+    apiUrl: 'https://miracidial-arboreally-tanisha.ngrok-free.dev -> http://localhost:5000 '  // แทนที่ด้วย URL จาก ngrok
 };
