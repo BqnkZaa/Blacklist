03e6c36decc19cc0409562eb9b95f33c977a57b3
badree <ayumguring093@gmail.com>
2025-12-01 23:19:07 +0700

    Fix: Add dialectModule to Sequelize config for Vercel deployment

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -7,7 +7,9 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
+        dialectModule: require('mysql2'), // Force mysql2 to be bundled
         logging: false,
         pool: {
             max: 5,
