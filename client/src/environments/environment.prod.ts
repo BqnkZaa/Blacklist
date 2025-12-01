b0713b53a3db72a9e11d601a9fe43fb04423bf3a
badree <ayumguring093@gmail.com>
2025-12-01 21:02:22 +0700

    Fix: Add /api path to ngrok URL in production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://miracidial-arboreally-tanisha.ngrok-free.dev'
+    apiUrl: 'https://miracidial-arboreally-tanisha.ngrok-free.dev/api'
 };
